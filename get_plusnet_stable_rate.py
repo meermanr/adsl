1a25177714377ae9c2d01668ca5b82543b2b8b19
Robert Meerman <meermanr@ikari.robmeerman.co.uk>
2009-11-12 00:08:53 +0000

    Initial versions

---
+++ get_plusnet_stable_rate.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+cmd = """curl "https://portal.plus.net/my.html?action=stable_rate" --cookie-jar ./cookiejar --cookie ./cookiejar --data 'username=meerman&authentication_realm=portal.plus.net&password=portgentil' --silent"""
+
+os.chdir( os.path.dirname( __file__ ) )
+
+if os.path.exists("cookiejar"):
+	os.unlink("cookiejar")
+
+os.system(cmd + " >/dev/null")		# Once to get a session cookie (stored in ./cookiejar)
+p = os.popen(cmd)	# And again with cookies obtained in previous step
+html = p.read()
+p.close()
+
+if os.path.exists("cookiejar"):
+	os.unlink("cookiejar")
+
+
+import re
+for section in re.split("</?dl>", html):
+	if "Current line speed:" in section:
+		for item in section.split("<dt>"):
+			if "</dt>" not in item:
+				continue
+
+			key, value = item.split("</dt>")
+			key = key.strip()
+			if key != "Current line speed:":
+				continue
+
+			value = re.sub("<.*?>", "", value)
+			value = value.strip()
+			print value
+			exit()
+		break
+else:
+	print >> sys.stderr, "Current line speed not present, script may need updating."
+	exit(1)
---
+++ get_sync_rates.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# Query the current ADSL WAN sync speed of my ZyXEL ADSL Modem
+
+password=""
+modem_ip="192.168.1.1"
+
+import telnetlib
+
+up = None
+down = None
+
+try:
+	tn = telnetlib.Telnet(modem_ip, 23, 5)
+	tn.read_until("Password: ", 5)
+	tn.write(password + "\n")
+	tn.read_until("modem> ", 5)
+	tn.write("wan adsl chandata\n")
+	s = tn.expect(["modem> "])[2]
+	tn.write("exit\n")
+	tn.read_all()
+
+	for line in s.replace("\r", "").split("\n"):
+		rate = None
+		try:
+			rate = int(line.split(":")[1].replace(" kbps", "").strip())
+		except: pass
+
+		if line.startswith("near-end"):
+			down = max(down, rate)
+		if line.startswith("far-end"):
+			up = max(up, rate)
+
+except EOFError:
+	tn.close()
+
+print "%s\t%s" % (up, down)
