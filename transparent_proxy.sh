22729332f93c2d6915719cc321143fea75f7bd1e
Robert Meerman <meermanr@ikari.robmeerman.co.uk>
2009-11-13 08:46:37 +0000

    Snapshot of useful iptables. Needs scriptifying

---
+++ transparent_proxy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#sudo iptables -t nat -S
+#-P PREROUTING ACCEPT
+#-P POSTROUTING ACCEPT
+#-P OUTPUT ACCEPT
+#-A PREROUTING ! -d 192.168.1.0/24 -p tcp -m tcp --dport 80 -j REDIRECT --to-ports 3128 
+
