656a23197f59d93ed684cc55d44f7a0555f375e7
Robert Meerman <robert.meerman@gmail.com>
2012-03-03 23:33:34 +0000

    Update get_plusnet_stable_rate.py

--- rrdlogger/get_plusnet_stable_rate.py
+++ rrdlogger/get_plusnet_stable_rate.py
@@ -3,22 +3,43 @@
 def get_ip_profile():
 	import os
 	import sys
+	import subprocess as sp
 
-	cmd = """curl "https://portal.plus.net/my.html?action=stable_rate" --cookie-jar ./cookiejar --cookie ./cookiejar --data 'username=meerman&authentication_realm=portal.plus.net&password=portgentil' --silent"""
+	base_cmd = [
+			"curl", "https://portal.plus.net/my.html?action=stable_rate",
+			"--data", "username=meerman&authentication_realm=portal.plus.net&password=portgentil&x=21&y=15",
+            "--silent",
+			]
 
 	os.chdir( os.path.dirname( __file__ ) )
 
 	if os.path.exists("cookiejar"):
 		os.unlink("cookiejar")
 
-	os.system(cmd + " >/dev/null")		# Once to get a session cookie (stored in ./cookiejar)
-	p = os.popen(cmd)	# And again with cookies obtained in previous step
-	html = p.read()
-	p.close()
+	# Once to get a session cookie (stored in ./cookiejar)
+	new_cmd = base_cmd + ["--cookie-jar", "./cookiejar"]
+	with file(os.devnull, "w+b") as fh:
+		ph = sp.Popen(
+				new_cmd,
+				stdin=fh,
+				stdout=fh,
+				stderr=fh,
+				shell=False,
+				)
+		ph.wait()
 
-	if os.path.exists("cookiejar"):
-		os.unlink("cookiejar")
-
+	# And again with cookies obtained in previous step
+	new_cmd = base_cmd + ["--cookie", "./cookiejar"]
+	with file(os.devnull, "w+b") as fh:
+		ph = sp.Popen(
+				new_cmd,
+				stdin=fh,
+				stdout=sp.PIPE,
+				stderr=fh,
+				shell=False,
+				)
+		html = ph.stdout.read()
+		ph.wait()
 
 	import re
 	for section in re.split("</?dl>", html):
@@ -34,10 +55,16 @@ def get_ip_profile():
 
 				value = re.sub("<.*?>", "", value)
 				value = value.strip()
-				return int(value)
+				# E.g. "3.5 Mb"
+				if " Mb" in value:
+					return int( float(value[:-len(" Mb")]) * 1000 )
 			break
 	else:
 		raise Exception("Current line speed not present, script may need updating.")
 
 if __name__ == "__main__":
-	print get_ip_profile()
+	try:
+		print get_ip_profile()
+	except:
+		print "U"
+		raise
