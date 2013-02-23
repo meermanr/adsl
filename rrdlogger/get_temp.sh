ceaf2f621587b1681b8ee7c1838c378ad4ab4b2f
Robert Meerman <robert.meerman@gmail.com>
2013-02-23 11:04:03 +0000

    Fix temperature scraper to support negative temperatures

--- rrdlogger/get_temp.sh
+++ rrdlogger/get_temp.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl --silent http://www.cl.cam.ac.uk/research/dtg/weather/current-obs.txt | sed -ne 's/^Temperature:\s*\([0-9.]*\).*/\1/p'
+curl --silent http://www.cl.cam.ac.uk/research/dtg/weather/current-obs.txt | sed -ne 's/^Temperature:\s*\([0-9.-]*\).*/\1/p'
