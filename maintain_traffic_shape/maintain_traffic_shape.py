97e324e4a00077e4bea4026167e9815a13cff526
Robert Meerman <robert.meerman@gmail.com>
2013-09-22 01:11:57 +0100

    Rewrite wondershaper to use iptables to classify LOCAL and INTERNET->LAN traffic. Cannot get either tc or iptables to classify LAN->INTERNET traffic for some reason.

--- maintain_traffic_shape/maintain_traffic_shape.py
+++ maintain_traffic_shape/maintain_traffic_shape.py
@@ -49,22 +49,11 @@ def maintain_traffic_shape():
         print "sync_up", sync_up
         return
 
-    ip_profile = int(ip_profile)
+    sync_down = int(sync_down / 1000)   # kbit -> Mbit
+    sync_up = int(sync_up / 1000)       # kbit -> Mbit
 
-    # Speed tests have demonstrated that the maximum achievable utilisation of 
-    # the DOWN link is about 80% (2.38Mbit/3MBit) (using the IP profile rather 
-    # than the ADSL sync speed).
-    sync_down = int(sync_down / 1000 * 0.80)
-
-    # http://speedtest.net has demonstrated that the upper utilisation of the 
-    # UP link is 85%: 380kbit / 448kbit. So to take control of things we need 
-    # to limit more than this.
-    sync_up = int(sync_up / 1000 * 0.85)
-
-    # In order for traffic shaping and policing to be effective, we need to 
-    # reign in the limits so *we* are the ones dropping and queuing packed,
-    limit_down = int(min(ip_profile, sync_down) * 0.90)
-    limit_up = int(sync_up * 0.90)
+    limit_down = min(ip_profile, sync_down)
+    limit_up = sync_up
 
     cmd = "sudo ../wondershaper/wondershaper eth0 %d %d" % (
             limit_down, limit_up)
